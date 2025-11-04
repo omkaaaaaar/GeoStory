1f1778a2150314aef9a0e2ce3a4b8e3f2e6c039b
omkaaaaaar <omkarpatkarr@gmail.com>
2025-11-04 19:59:08 +0530

    w

--- geostory_backend/geostory_backend/settings.py
+++ geostory_backend/geostory_backend/settings.py
@@ -47,12 +47,18 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = 'geostory_backend.urls'
@@ -79,10 +85,10 @@ WSGI_APPLICATION = 'geostory_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        engine='django.contrib.gis.db.backends.postgis'
+    )
 }
 
 
